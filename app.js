41a125ec596119e5df3a65424e54f84a400d6375
Eredis <codelinehd@gmail.com>
2017-09-03 16:35:25 -0400

    Handlebars file extensions are now .hbs.

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ import exphbs from 'express-handlebars';
 
 const app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+app.engine('hbs', exphbs({extname: '.hbs', defaultLayout: '_layout'}));
+app.set('view engine', 'hbs');
 
 app.get('/', function (req, res) {
     res.render('home');
