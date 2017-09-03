51144239a43a008fbeecae2c8dda37ee684c11e4
Eredis <codelinehd@gmail.com>
2017-09-03 16:24:45 -0400

    Added Handlebars View Engine

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import exphbs from 'express-handlebars';
 
 const app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
 
 app.listen(3000, () => console.log("Running on port 3000"));
