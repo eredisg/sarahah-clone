bfa0d9624b5dcd1618bb2d8ecb53a12d1432dd29
Eredis <codelinehd@gmail.com>
2017-09-03 15:09:39 -0400

    Added Dev Dependencies

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public/css/'))
+});
+
+gulp.task('default', function() {
+    gulp.watch('sass/**/*.scss', ['styles']);
+});
