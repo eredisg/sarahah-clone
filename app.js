1e84f72e81d9eacfea68a6ee2aa9ac1c0520c93a
Eredis <codelinehd@gmail.com>
2017-09-08 00:18:36 -0400

    Added Express Flash

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import exphbs from 'express-handlebars';
 import session from 'express-session';
+import flash from 'express-flash';
 import passport from 'passport';
 import config from 'config';
 import router from './routes/routes';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 
 const app = express();
@@ -14,12 +16,15 @@ mongoose.connect(config.database.uri);
 app.engine('hbs', exphbs({extname: '.hbs', defaultLayout: '_layout'}));
 app.set('view engine', 'hbs');
 
-app.use(express.static('public'));
+app.use('/public', express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
     secret: config.session.secret,
     resave: true,
     saveUninitialized: true
 }));
+app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,26 +1,65 @@
 import express from 'express';
 const router = express.Router();
 
+import User from '../models/user';
 import passport from '../auth/github';
 
-router.get('/', (req, res, next) => {
-    res.render('index', {user:req.user});
+router.get('/', (req, res) => {
+    res.render('index', { user: req.user });
 });
 
-router.get('/profile', (req, res, next) => {
+router.get('/profile', (req, res) => {
     res.render('profile');
 });
 
-router.get('/login', (req, res, next) => {
-    res.send('Go back and register!');
-  });
+router.get('/:user/submit', (req, res) => {
+    const usr = req.params.user;
 
-  router.get('/auth/github', passport.authenticate('github', { scope: [ 'user:email' ] }));
-  
-  router.get('/auth/github/callback',
+    User.findOne({ name: usr }, (err, user) => {
+        if (err) {
+            req.flash('error', 'A server side error occured. Please try again in a few minutes.')
+            res.render('index', { failure: req.flash('error') })
+        } else if (!user || usr !== user.name) {
+            req.flash('404', 'User ' + usr + ' could not be found')
+            res.render('index', { failure: req.flash('404') })
+        }  else {
+            res.render('submit', { user });
+        }
+    })
+})
+
+router.post('/:user/submit', (req, res) => {
+    const usr = req.params.user;
+    const comment = req.body.comment;
+
+    User.findOne({ name: usr }, (err, user) => {
+        if (err) {
+            req.flash('error', 'A server side error occured. Please try again in a few minutes.')
+            res.render('index', { failure: req.flash('error') })
+        } else if (!user || usr !== user.name) {
+            req.flash('404', 'User ' + usr + ' could not be found')
+            res.render('index', { failure: req.flash('404') })
+        } else {
+            user.comments.push({ comment, createdAt: Date.now() });
+            user.save()
+            .then((user) => {
+                req.flash('commentSent', 'You have submitted a comment to ' + user.name)
+                res.render('Submit', { user, success: req.flash('commentSent')});
+            });
+        }
+    })
+});
+
+router.get('/login', (req, res) => {
+    res.send('Go back and register!');
+});
+
+router.get('/auth/github', passport.authenticate('github', { scope: ['user:email'] }));
+
+router.get('/auth/github/callback',
     passport.authenticate('github', { failureRedirect: '/login' }),
-    function(req, res) {
-      res.redirect('/');
+    function (req, res) {
+        res.redirect('/');
     });
 
 export default router;
